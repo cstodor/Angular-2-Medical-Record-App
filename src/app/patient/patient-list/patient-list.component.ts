b13658c5413fe5bdddd4006edb6c29f2aa412866
Csaba Todor <todorcs@gmail.com>
2017-01-18 16:53:31 +0200

    Listed the Patients on the PatientList Component
    - imported PatientService and IPatient interface
    - subscribed to the PatientService's getPatients() method and assigned
    the returned patients to the local _patients array

--- src/app/patient/patient-list/patient-list.component.ts
+++ src/app/patient/patient-list/patient-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PatientService } from '../patient.service';
+import { IPatient } from '../../../api/patient';
+
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientListComponent implements OnInit {
 
-  constructor() { }
+  _patients: IPatient[];
+  errorMessage: string;
+
+  constructor(private _patientService: PatientService) { }
 
   ngOnInit() {
+    this._patientService.getPatients()
+      .subscribe(
+      _patients => this._patients = _patients,
+      error => this.errorMessage = <any>error);
   }
 
 }
