c39a113a45a9ed71f4979a9c929cc44ae8ea5288
Csaba Todor <todorcs@gmail.com>
2017-01-19 12:50:04 +0200

    Listed Patient Info for Selected Patient
    + Updated Patient Info CSS
    + Added Input Field for Patient Name

--- src/app/patient/patient-details/patient-info/patient-info.component.ts
+++ src/app/patient/patient-details/patient-info/patient-info.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+// Service
+import { PatientService } from '../../patient.service';
+// Interface
+import { IPatient } from '../../../../api/patient';
 
 @Component({
   selector: 'app-patient-info',
   templateUrl: './patient-info.component.html',
   styleUrls: ['./patient-info.component.css']
 })
-export class PatientInfoComponent implements OnInit {
+export class PatientInfoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  _patients: IPatient[];
+  _patient: IPatient;
+  private sub: Subscription;
+
+  constructor(
+    private _patientService: PatientService,
+    private _route: ActivatedRoute,
+    private _router: Router) { }
 
   ngOnInit() {
+    // Get Single Patient
+    this.sub = this._route.params.subscribe(
+      (params: any) => {
+        let id = +params['id'];
+        if (id) {
+          this._patientService.getPatient(id).subscribe(_patient => {
+            this._patient = _patient;
+          });
+        }
+      }
+    );
   }
-
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
