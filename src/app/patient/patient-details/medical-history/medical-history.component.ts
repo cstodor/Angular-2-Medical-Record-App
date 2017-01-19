7faf028e2313ca42809a5f600aca8abd0da8565a
Csaba Todor <todorcs@gmail.com>
2017-01-19 12:47:52 +0200

    Listed Medical History for Selected Patients

--- src/app/patient/patient-details/medical-history/medical-history.component.ts
+++ src/app/patient/patient-details/medical-history/medical-history.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+// Service
+import { PatientService } from '../../patient.service';
+// Interface
+import { IPatient } from '../../../../api/patient';
 
 @Component({
   selector: 'app-medical-history',
   templateUrl: './medical-history.component.html',
   styleUrls: ['./medical-history.component.css']
 })
-export class MedicalHistoryComponent implements OnInit {
+export class MedicalHistoryComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  _patients: IPatient[];
+  _patient: IPatient;
+  patiendIndex: number = 1;
+  private sub: Subscription;
+  errorMessage: string;
+  
+  constructor(
+    private _patientService: PatientService,
+    private _route: ActivatedRoute,
+    private _router: Router) { }
 
   ngOnInit() {
+    // Get All PatientService
+    this._patientService.getPatients()
+      .subscribe(
+      _patients => this._patients = _patients,
+      error => this.errorMessage = <any>error);
+    // Get Single Patient
+    this.sub = this._route.params.subscribe(
+      (params: any) => {
+        let id = +params['id'];
+        this.patiendIndex = +params['id'];
+        console.log("PI:" + this.patiendIndex);
+        if (id) {
+          this._patientService.getPatient(id).subscribe(_patient => {
+            this._patient = _patient;
+          });
+        }
+      }
+    );
   }
-
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
